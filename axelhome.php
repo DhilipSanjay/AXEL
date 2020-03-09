8431e3c651f5e214cf06b4c856af3de0b88026a2
MC007 <43838718+maverickcoders007@users.noreply.github.com>
2020-03-09 19:12:35 +0530

    Add files via upload
    Adding homepage.php and related files

---
+++ axelhome.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<head>
+<title>Axel - Home</title>
+<link rel="stylesheet" href="home.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<link rel="icon" href="logo.png">
+<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,200i,300,300i,400,400i&display=swap" rel="stylesheet">
+<script src="home.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div id="home">
+
+<img id="logo" src="logo.png" height="47px" width="45px">
+<div id="title">AXEL</div>
+<img id="user" src="user.png">
+<img id="listicon" src="list.png">
+
+<div id="maininfo">
+    <div id="content">
+     <div id="text">It's time to <p id="evolve">evolve.</p></div>
+        <div id="info">
+        Whether you are a budding startup looking for mentorship from the best experts or an experienced mentor with a 
+        passion to guide startups, we are here to help you out.
+        </div>
+
+        <div id="buttonholder">
+        <div class="button" onmouseover="highlightevolve()" onmouseout="dehighlightevolve()">I am a startup</div>
+        <div class="button" onmouseover="highlightevolve()" onmouseout="dehighlightevolve()">I am a mentor</div>
+        </div>
+
+    </div>
+</div>
+</div>
+
+<div id="others">
+
+<div class="label pollstitle">Poll of the day</div>
+<div id="polls">
+Polls go here
+</div>
+
+<div class="label">News</div>
+<hr width="95%">
+<div id="news">
+
+<div class="newsholder">
+    <div class="imgholder">
+</div>
+<div class="newsinfo"><p>Content goes here</p></div>
+
+</div>
+
+<div class="newsholder">
+    <div class="imgholder">
+</div>
+<div class="newsinfo"><p>Content goes here</p></div>
+
+</div>
+
+<div class="newsholder">
+    <div class="imgholder">
+</div>
+<div class="newsinfo"><p>Content goes here</p></div>
+
+</div>
+
+<div class="newsholder">
+    <div class="imgholder">
+    </div>
+<div class="newsinfo"><p>Content goes here</p></div>
+</div>
+
+</div>
+
+</div>
+</body>
+</html>
---
+++ home.js
@@ -0,0 +1,10 @@
+function highlightevolve()
+{
+    document.getElementById("evolve").style.color="#85C1E9";
+}
+
+
+function dehighlightevolve()
+{
+    document.getElementById("evolve").style.color="white";
+}
