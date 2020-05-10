16a65298f4a03cb81cd7abd9f7a76131a03de11c
DhilipSanjay <dhilip.sanjay@yahoo.com>
2020-05-10 22:29:31 +0530

    Added restriction for general user to host contest

--- dashboard.php
+++ dashboard.php
@@ -156,9 +156,6 @@ $(function() {
       <div id="close" onclick="opennewannouncementbox(<?php echo $userid ?>)">Close</div>
 </div>
 
-
-
-
 <div id="createnewcontest">
       <div id="title">Host a new Contest</div>
 
@@ -340,13 +337,22 @@ $temp=0;
     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 002.5 15h11a1.5 1.5 0 001.5-1.5v-6a.5.5 0 00-1 0v6a.5.5 0 01-.5.5h-11a.5.5 0 01-.5-.5v-11a.5.5 0 01.5-.5H9a.5.5 0 000-1H2.5A1.5 1.5 0 001 2.5v11z" clip-rule="evenodd"/>
     </svg>
     Create a new announcement</div>
+
+    <?php
+    $generaluser_query = "SELECT * FROM generaluser WHERE generaluserid = " . $userid;
+    $generaluser_result= mysqli_query($conn, $generaluser_query);
+    if(mysqli_num_rows($generaluser_result) === 0)
+    {
+    ?>
     <div class="topbuttons topcreatecontestbutton" onclick="opennewcontestbox()">
     <svg class="bi bi-award" style="margin-right:10px" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" d="M9.669.864L8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193l-1.51-.229L8 1.126l-1.355.702-1.51.229-.684 1.365-1.086 1.072L3.614 6l-.25 1.506 1.087 1.072.684 1.365 1.51.229L8 10.874l1.356-.702 1.509-.229.684-1.365 1.086-1.072L12.387 6l.248-1.506-1.086-1.072-.684-1.365z" clip-rule="evenodd"/>
     <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
     </svg>
     Host a new contest</div>
-
+    <?php
+    }
+    ?>
     <!--While creating make a new announcement button, check out materialize open source css framework -->
     
     <div class="label">Announcements</div>
