ab8bd4bc09a783e08b77f3c40daf08663bae2cf5
Indrajit <indrajitvijayakumar@gmail.com>
2020-05-24 14:27:40 +0530

    UI changes in login!

--- login.php
+++ login.php
@@ -38,21 +38,70 @@ if(isset($_POST["login"]))
 <link rel="stylesheet" href="https://unpkg.com/@clr/ui@0.12.5/clr-ui.min.css" />
 </head>
 
+<style>
+.loginbut
+{
+    background-color: #76D7C4 !important;
+}
+
+.loginbut:hover,.loginbut:focus
+{
+    color:#76D7C4 !important;
+    background-color:white !important;
+    border:1px solid #76D7C4 !important;
+    outline:none !important;
+    box-shadow:none !important;
+}
+
+.login-wrapper
+{
+    opacity:0.2;
+    transform:translateX(-75px);
+    animation-name:login !important;
+    animation-duration:0.5s !important;
+    animation-fill-mode:forwards !important;
+}
+
+.login
+{
+    transform:translateX(-100px);
+    animation-name:login !important;
+    animation-duration:0.5s !important;
+    opacity:0.2;
+    animation-fill-mode:forwards !important;
+}
+
+#email:focus,#pass:focus
+{
+    background:none !important;
+    background-size:0 0 !important;
+    border-bottom:1px solid #76D7C4 !important;
+}
+
+@keyframes login {
+    100%
+    {
+        transform:translateX(0);
+        opacity:1;
+    }
+}
+</style>
+
 <body>
     <div class="login-wrapper" style="background:url('regbg.jpg');background-position:center;background-attachment:fixed;background-repeat: no-repeat;background-size: cover">
         <form class="login" action="" method="post">
             <section class="title">
                 <h1 class="welcome">Login</h1>
-                <h4>Now is your time to evolve.</h4>
+                <h4>Now is your time to <span style="color:#76D7C4">evolve.</span></h4>
             </section>
             <div class="login-group">
                 <clr-input-container>
                 <label class="clr-sr-only">Email Address</label>
-                <input type="email" name="email" style="padding:20px 0;margin-bottom:20px;width:80%" clrInput placeholder="Email" [(ngModel)]="form.email" required/>
+                <input type="email" name="email" id="email" style="padding:20px 0;margin-bottom:20px;width:80%" clrInput placeholder="Email" [(ngModel)]="form.email" required/>
                 </clr-input-container>
                 <clr-password-container>
                 <label class="clr-sr-only">Password</label>
-                <input type="password" name="password" style="padding:20px 0;margin-bottom:20px;width:80%"  clrPassword placeholder="Password" [(ngModel)]="form.password" required/>
+                <input type="password" name="password" id="pass" style="padding:20px 0;margin-bottom:20px;width:80%"  clrPassword placeholder="Password" [(ngModel)]="form.password" required/>
                 </clr-password-container>
                 <clr-checkbox-wrapper>
                 <!--<label>Remember me</label>
@@ -66,8 +115,8 @@ if(isset($_POST["login"]))
                 <br>
                 <input type ="radio" id = "general" name="radio" value="general" required> General User
                 </clr-checkbox-wrapper>
-                <button type="submit" name="login" style="margin-top:25px;width:80%;border:none" class="btn btn-primary">Login</button>
-                <a href="register.html"  style="margin-top:15px;width:80%" class="signup">I want to create a new account!</a>
+                <button type="submit" name="login" style="margin-top:25px;width:80%;border:none" class="btn btn-primary loginbut">Login</button>
+                <a href="register.html"  style="margin-top:15px;width:80%;color:#76D7C4 !important" class="signup">I want to create a new account!</a>
             </div>
         </form>
     </div>
