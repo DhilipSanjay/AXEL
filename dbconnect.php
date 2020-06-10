40278a3c644497bea8fe868ffd0774f78a75368a
Indrajit <indrajitvijayakumar@gmail.com>
2020-06-10 12:55:11 +0530

    change

--- dbconnect.php
+++ dbconnect.php
@@ -9,8 +9,8 @@ $conn=mysqli_connect($servername,$username,$password,$database);
 
 if(!$conn)
 {
-    /*die("Connection error: " . mysqli_connect_errno());*/
-    header('location:error.php');
+    die("Connection error: " . mysqli_connect_errno());
+    //header('location:error.php');
 }
 
 mysqli_set_charset($conn,"utf8");
